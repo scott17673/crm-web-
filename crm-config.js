16af600d41de6f58963fe0b23142fb8826173b86
scott17673 <scott17673@users.noreply.github.com>
2026-04-13 13:30:37 -0400

    Add Lead Finder tab with industry picker, cities section, live logs and rows

---
+++ crm-config.js
@@ -0,0 +1,6 @@
+window.CRM_CONFIG = {
+  "appLabel": "Ed Industrial CRM Copy",
+  "supabaseUrl": "https://dqqitnvyuebqfvgplcba.supabase.co",
+  "supabaseAnonKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxcWl0bnZ5dWVicWZ2Z3BsY2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNDcwNDUsImV4cCI6MjA4ODcyMzA0NX0.15H-Ry876rY-Hl4vPzY5DZ95th_qqjDAqqVKlTSMeXY",
+  "finderDashboardUrl": "http://localhost:8780"
+};
