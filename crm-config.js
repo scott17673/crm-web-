d2dfe25d91971263e2577bcb5633ba27a657fd9a
scott17673 <scott17673@users.noreply.github.com>
2026-05-14 09:14:55 -0400

    Fix finder dashboard URL

--- crm-config.js
+++ crm-config.js
@@ -2,5 +2,5 @@ window.CRM_CONFIG = {
   "appLabel": "Ed Industrial CRM Copy",
   "supabaseUrl": "https://dqqitnvyuebqfvgplcba.supabase.co",
   "supabaseAnonKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxcWl0bnZ5dWVicWZ2Z3BsY2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNDcwNDUsImV4cCI6MjA4ODcyMzA0NX0.15H-Ry876rY-Hl4vPzY5DZ95th_qqjDAqqVKlTSMeXY",
-  "finderDashboardUrl": "http://10.0.0.114:8780"
+  "finderDashboardUrl": "http://localhost:8780"
 };
